7e6977ec3f8d511114641c6d181a329a41460409
Maks Nikulnikov <5286433@gmail.com>
2022-10-18 19:15:10 +0300

    hotfix/after first revue fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,6 @@
 const popupElement = document.querySelector('.popup');
 const popupButtonOpen = document.querySelector('.profile__edit-button');
 const popupButtonClose = popupElement.querySelector('.popup__close-btn');
-const popupButtonSave = popupElement.querySelector('.popup__submit-btn');
 const popupTextFieldTitle = popupElement.querySelector('.popup__text_type_name');
 const popupTextFieldSubtitle = popupElement.querySelector('.popup__text_type_caption');
 const profileTextFieldTitle = document.querySelector('.profile__title');
@@ -9,26 +8,22 @@ const profileTextFieldSubitle = document.querySelector('.profile__subtitle');
 const form = document.querySelector('.popup__container');
 
 const openPopup = function () {
-    popupElement.classList.toggle('popup_opened');
+    popupElement.classList.add('popup_opened');
     popupTextFieldTitle.value = profileTextFieldTitle.textContent;
     popupTextFieldSubtitle.value = profileTextFieldSubitle.textContent;
 }
 
-popupButtonOpen.addEventListener('click', openPopup);
-
 const closePopup = function () {
     popupElement.classList.remove('popup_opened');
-    popupTextFieldTitle.value = '';
-    popupTextFieldSubtitle.value = '';
 }
-
-popupButtonClose.addEventListener('click', closePopup);
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileTextFieldTitle.textContent = popupTextFieldTitle.value;
     profileTextFieldSubitle.textContent = popupTextFieldSubtitle.value;
-    popupElement.classList.remove('popup_opened');
+    closePopup();
 }
 
+popupButtonOpen.addEventListener('click', openPopup);
+popupButtonClose.addEventListener('click', closePopup);
 form.addEventListener('submit', formSubmitHandler); 
