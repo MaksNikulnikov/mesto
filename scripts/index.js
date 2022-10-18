8704a7de6f997c31fdb2429bf063f729627e5fb6
Maks Nikulnikov <5286433@gmail.com>
2022-10-18 11:00:40 +0300

    feat add js to popup

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const popupElement = document.querySelector('.popup');
+const popupButtonOpen = document.querySelector('.profile__info_edit-button');
+const popupButtonClose = popupElement.querySelector('.popup__close-btn');
+const popupButtonSave = popupElement.querySelector('.popup__submit-btn');
+const popupTextFieldTitle = popupElement.querySelector('.popup__text_type_name');
+const popupTextFieldSubtitle = popupElement.querySelector('.popup__text_type_caption');
+const profileTextFieldTitle = document.querySelector('.profile__info_title');
+const profileTextFieldSubitle = document.querySelector('.profile__info_subtitle');
+const form = document.querySelector('.popup__container');
+
+const openPopup = function () {
+    popupElement.classList.toggle('popup_opened');
+    popupTextFieldTitle.placeholder = profileTextFieldTitle.textContent;
+    popupTextFieldSubtitle.placeholder = profileTextFieldSubitle.textContent;
+}
+
+popupButtonOpen.addEventListener('click', openPopup);
+
+const closePopup = function (){
+    popupElement.classList.remove('popup_opened');
+    popupTextFieldTitle.value = '';
+    popupTextFieldSubtitle.value = '';
+}
+
+popupButtonClose.addEventListener('click', closePopup);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTextFieldTitle.textContent = popupTextFieldTitle.value;
+    profileTextFieldSubitle.textContent = popupTextFieldSubtitle.value;
+    popupElement.classList.remove('popup_opened');
+}
+
+form.addEventListener('submit', formSubmitHandler); 
