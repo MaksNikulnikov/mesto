080462de516133c5a9725d6b1bb5fcb401d76f4c
Maks Nikulnikov <5286433@gmail.com>
2023-01-06 12:26:36 +0300

    Updates

--- main.js
+++ main.js
@@ -133,7 +133,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
 function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
-var Card = /*#__PURE__*/_createClass(function Card(cardData, cardsTemplateSelector, handleCardClick) {
+var Card = /*#__PURE__*/_createClass(function Card(cardData, cardsTemplateSelector, handleCardClick, handleRemoveClick) {
   var _this = this;
   _classCallCheck(this, Card);
   _defineProperty(this, "_addLikeListener", function () {
@@ -148,7 +148,10 @@ var Card = /*#__PURE__*/_createClass(function Card(cardData, cardsTemplateSelect
   });
   _defineProperty(this, "_addRemoveListener", function () {
     var buttonDelete = _this._card.querySelector('.element__delete');
-    buttonDelete.addEventListener('click', _this._removeCard);
+    // buttonDelete.addEventListener('click', this._removeCard);
+    buttonDelete.addEventListener('click', function () {
+      _this._handleRemoveClick(_this);
+    });
   });
   _defineProperty(this, "_addViewListener", function () {
     _this._image.addEventListener('click', function () {
@@ -175,6 +178,7 @@ var Card = /*#__PURE__*/_createClass(function Card(cardData, cardsTemplateSelect
   this._amountLikes = cardData.likes.length;
   this._cardsTemplate = document.querySelector(cardsTemplateSelector).content;
   this._handleCardClick = handleCardClick;
+  this._handleRemoveClick = handleRemoveClick;
 });
 
 
@@ -329,6 +333,71 @@ var Popup = /*#__PURE__*/function () {
   }]);
   return Popup;
 }();
+
+
+/***/ }),
+
+/***/ "./src/components/PopupRemoveElement.js":
+/*!**********************************************!*\
+  !*** ./src/components/PopupRemoveElement.js ***!
+  \**********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ PopupRemoveElement)
+/* harmony export */ });
+/* harmony import */ var _Popup_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./Popup.js */ "./src/components/Popup.js");
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
+function _get() { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get.bind(); } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(arguments.length < 3 ? target : receiver); } return desc.value; }; } return _get.apply(this, arguments); }
+function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); Object.defineProperty(subClass, "prototype", { writable: false }); if (superClass) _setPrototypeOf(subClass, superClass); }
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf ? Object.setPrototypeOf.bind() : function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function _createSuperInternal() { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } else if (call !== void 0) { throw new TypeError("Derived constructors may only return object or undefined"); } return _assertThisInitialized(self); }
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function () {})); return true; } catch (e) { return false; } }
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf.bind() : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+var PopupRemoveElement = /*#__PURE__*/function (_Popup) {
+  _inherits(PopupRemoveElement, _Popup);
+  var _super = _createSuper(PopupRemoveElement);
+  function PopupRemoveElement(popupSelector, remove) {
+    var _this;
+    _classCallCheck(this, PopupRemoveElement);
+    _this = _super.call(this, popupSelector);
+    _this._remove = remove;
+    _this._buttonRemove = _this._popup.querySelector('.popup__submit-btn');
+    return _this;
+  }
+  _createClass(PopupRemoveElement, [{
+    key: "setEventListeners",
+    value: function setEventListeners() {
+      var _this2 = this;
+      this._buttonRemove.addEventListener('click', function () {
+        _this2._remove(_this2._removedElement);
+      });
+      _get(_getPrototypeOf(PopupRemoveElement.prototype), "setEventListeners", this).call(this);
+    }
+  }, {
+    key: "setRemovedElement",
+    value: function setRemovedElement(removedElement) {
+      this._removedElement = removedElement;
+    }
+  }, {
+    key: "close",
+    value: function close() {
+      this._removedElement = null;
+      _get(_getPrototypeOf(PopupRemoveElement.prototype), "close", this).call(this);
+    }
+  }]);
+  return PopupRemoveElement;
+}(_Popup_js__WEBPACK_IMPORTED_MODULE_0__["default"]);
 
 
 /***/ }),
@@ -733,6 +802,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_UserInfo_js__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../components/UserInfo.js */ "./src/components/UserInfo.js");
 /* harmony import */ var _components_Section_js__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../components/Section.js */ "./src/components/Section.js");
 /* harmony import */ var _components_Api__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ../components/Api */ "./src/components/Api.js");
+/* harmony import */ var _components_PopupRemoveElement__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ../components/PopupRemoveElement */ "./src/components/PopupRemoveElement.js");
+
 
 
 
@@ -756,8 +827,16 @@ var handleCardClick = function handleCardClick(_ref) {
     caption: caption
   });
 };
+var popupRemoveCard = new _components_PopupRemoveElement__WEBPACK_IMPORTED_MODULE_10__["default"]('.popup_remove-card', function () {
+  popupRemoveCard.close();
+});
+var handleRemoveClick = function handleRemoveClick(removedElement) {
+  popupRemoveCard.setRemovedElement(removedElement);
+  popupRemoveCard.setEventListeners();
+  popupRemoveCard.open();
+};
 var getCard = function getCard(cardData) {
-  return new _components_Card_js__WEBPACK_IMPORTED_MODULE_3__["default"](cardData, '.element__template', handleCardClick).createCard();
+  return new _components_Card_js__WEBPACK_IMPORTED_MODULE_3__["default"](cardData, '.element__template', handleCardClick, handleRemoveClick).createCard();
 };
 var sectionCards = new _components_Section_js__WEBPACK_IMPORTED_MODULE_8__["default"]({
   items: [],
